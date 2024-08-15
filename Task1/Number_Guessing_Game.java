420bf2d1d74fd78890b0a0da39a7e06b17d2ed60
shubhamrawatsks1@gmail.com <shubhamrawatsks1@gmail.com>
2024-08-15 11:58:39 +0530

    Add files via upload

--- Task1/Number_Guessing_Game.java
+++ Task1/Number_Guessing_Game.java
@@ -1,16 +1,64 @@
-Number Guessing Game
-This is a simple Java-based Number Guessing Game where the player has to guess a randomly generated number within a specified range. The game provides the player with a limited number of attempts to guess the correct number. After each incorrect guess, the game gives a hint whether the correct number is higher or lower than the player's guess.
+import java.util.Random;
+import java.util.Scanner;
 
-Key Features:
+public class Number_Guessing_Game {
 
-The number to be guessed is randomly generated within a range of 1 to 100.
-The player has a maximum of 10 attempts to guess the correct number.
-The game informs the player if the guessed number is higher or lower than the target number.
-After running out of attempts or guessing correctly, the player is given the option to play again.
-How to Play:
+    private static final int MAX_TRIES = 10;
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 100;
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        
+        boolean playAgain = true;
 
-Run the game.
-The game will prompt you to guess a number between 1 and 100.
-Enter your guess and the game will provide feedback.
-Continue guessing until you either find the correct number or run out of attempts.
-After the game ends, you can choose to play again or exit.
+        while (playAgain) {
+            int secretNumber = generateRandomNumber(rand);
+            int remainingTries = MAX_TRIES;
+            boolean isGuessedCorrectly = false;
+            
+            System.out.println("Welcome to the Number Guessing Game!");
+            System.out.printf("Guess the number between %d and %d.%n", MIN_VALUE, MAX_VALUE);
+
+            while (remainingTries > 0 && !isGuessedCorrectly) {
+                System.out.printf("You have %d guesses left. Make your guess: ", remainingTries);
+                
+                int guess = getValidatedInput(scanner);
+                remainingTries--;
+
+                if (guess == secretNumber) {
+                    isGuessedCorrectly = true;
+                    System.out.println("Congratulations! You Guessed the Right Number.");
+                } else if (guess < secretNumber) {
+                    System.out.println("The number is higher. Try again!");
+                } else {
+                    System.out.println("The number is lower. Try again!");
+                }
+            }
+
+            if (!isGuessedCorrectly) {
+                System.out.printf("Out of attempts! The correct number was: %d%n", secretNumber);
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String userResponse = scanner.next().trim().toLowerCase();
+            playAgain = userResponse.equals("yes");
+        }
+
+        System.out.println("Thanks for playing! See you next time.");
+        scanner.close();
+    }
+
+    private static int generateRandomNumber(Random rand) {
+        return rand.nextInt(MAX_VALUE - MIN_VALUE + 1) + MIN_VALUE;
+    }
+
+    private static int getValidatedInput(Scanner scanner) {
+        while (!scanner.hasNextInt()) {
+            System.out.println("Please enter a valid number.");
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+}
